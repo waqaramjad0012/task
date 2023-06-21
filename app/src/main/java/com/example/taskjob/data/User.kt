96cb171ca201c57f1d6d9377b66a433473970377
waqar <waqaramjad0012@gmail.com>
2023-06-21 07:18:16 +0500

    initialize gitt

--- app/src/main/java/com/example/taskjob/data/User.kt
+++ app/src/main/java/com/example/taskjob/data/User.kt
@@ -4,4 +4,5 @@ import java.io.Serializable
 
 data class User( var name:String?=null, var id:String?=null,
                  val email: String?=null, var password: String?=null,
-                 val lat:Double?=null, val longitude:Double?=null):Serializable
+                 val lat:Double?=null, val longitude:Double?=null)
+    :Serializable
